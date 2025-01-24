37329bff679f0dcb0de2a9f5c0162470aff6b38c
Alexandr Yershov <69073713+Alexandr886@users.noreply.github.com>
2025-01-24 11:47:18 +0700

    Пофиксин баг: с мобильной клавиатуры часто добавляется пробел в конце. Почистил все пробелы перед поиском города

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 async function getWeather() {
-    document.getElementById('weatherInfo').innerText = 'Ждите';
-    const city = document.getElementById('city').value;
-    const API_KEY = 'f521b8ecf5054c1bdc877cb11fdc6e68';
+  document.getElementById('weatherInfo').innerText = 'Ждите';
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${API_KEY}&units=metric`);
+  //получаем полное значение из инпута
+  const cityValue = document.getElementById('city').value;
 
-    const data = await response.json();
+  //убираем пробелы (на мобилке часто в конец добавляется пробел)
+  const city = cityValue.replace(/\s/g, "");
 
-    if (data.cod === 200) {
-        document.getElementById('weatherInfo').innerHTML = `
-        <h2>${data.name}</h2>
-        <p>Температура: <span>${data.main.temp}°C</span></p>
-        <p>Влажность: <span>${data.main.humidity}%</span></p>
-        <p>Состояние: <span>${data.weather[0].description}</span></p>
-        `;
-    } else document.getElementById('weatherInfo').innerText = 'Такого города нет или допущена ошибка при вводе.';
+  const API_KEY = 'f521b8ecf5054c1bdc877cb11fdc6e68';
+
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${API_KEY}&units=metric`);
+
+  const data = await response.json();
+
+  if (data.cod === 200) {
+    document.getElementById('weatherInfo').innerHTML = `
+    <h2>${data.name}</h2>
+    <p>Температура: <span>${data.main.temp}°C</span></p>
+    <p>Влажность: <span>${data.main.humidity}%</span></p>
+    <p>Состояние: <span>${data.weather[0].description}</span></p>
+    `;
+  } else document.getElementById('weatherInfo').innerText = 'Такого города нет или допущена ошибка при вводе.';
 }
+
+
+
 
 
 
